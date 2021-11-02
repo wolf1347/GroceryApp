2a78394e43b173e2d490f9349eea678b36f72203
Amber Wolf <60445098+wolf1347@users.noreply.github.com>
2021-11-02 07:50:03 -0600

    Initial project files
    Initial project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GroceryApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
